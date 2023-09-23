1afbe32730dd83840e28e8b1cde88ac81448a019
Marton Pal <h161409@stud.u-szeged.hu>
2023-09-23 21:43:33 +0200

    Media queries 1.0 | Mobile navbar

--- resources/scripts/show_mobile_navbar.js
+++ resources/scripts/show_mobile_navbar.js
@@ -1,3 +1,7 @@
-function showMobileNavbar(){
+let open = false
+var rs = getComputedStyle(document.querySelector(':root'));
 
+function showMobileNavbar(){
+    open = !open
+    document.getElementById("mobile-grid-items").style.right = (open ? 0 : "-300%")
 }
