69382c65f20cd4e710b249494bbc69e19fddda34
Marton Pal <h161409@stud.u-szeged.hu>
2023-09-23 02:08:22 +0200

    Header 1.2

--- resources/scripts/header_slider.js
+++ resources/scripts/header_slider.js
@@ -1,8 +1,9 @@
 let headerSliderID = 0
-let sliderTime = 10 //sec
+let sliderTime = 3 //sec
+let bgID = true
 
 const setHeaderBG = async () => {
-    const response = await fetch("https://api.slingacademy.com/v1/sample-data/photos?offset=31&limit=5")
+    const response = await fetch("https://api.slingacademy.com/v1/sample-data/photos?offset=110&limit=5")
     const data = await response.json()
 
     slideHeader(data)
@@ -10,10 +11,18 @@ const setHeaderBG = async () => {
 }
 
 function slideHeader(data){
-    document.getElementById("header").style.backgroundImage = "url(" + data.photos[headerSliderID].url + ")"
+    document.getElementById("header-bg-"+(bgID ? 1 : 2)).style.display = "block"
+    document.getElementById("header-bg-"+(bgID ? 2 : 1)).style.display = "none"
+
+    document.getElementById("header-bg-"+(bgID ? 1 : 2)).style.backgroundImage = "url(" + data.photos[headerSliderID].url + ")"
     document.getElementById("header-slider-index").innerHTML = (headerSliderID+1)+"/5"
+
     headerSliderID++
     if (headerSliderID === 5) headerSliderID = 0
+    bgID = !bgID
+
+    document.getElementById("header-bg-"+(bgID ? 1 : 2)).style.backgroundImage = "url(" + data.photos[headerSliderID].url + ")"
+
     document.getElementById("header-slider-line").animate(
         [
             { width: '0'},
