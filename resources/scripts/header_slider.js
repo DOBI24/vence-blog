ae18da80ecd95b8fca191ca7ff21c4de233bd839
Marton Pal <h161409@stud.u-szeged.hu>
2023-09-25 02:11:49 +0200

    Sliders 2.0

--- resources/scripts/header_slider.js
+++ resources/scripts/header_slider.js
@@ -1,25 +1,15 @@
+let sliderTime = 5 //sec
 let headerSliderID = 0
-let sliderTime = 10 //sec
 let bgHeaderID = true
+let h_animationIndex = 0
 
 const setHeaderBG = async () => {
-    const response = await fetch("https://api.slingacademy.com/v1/sample-data/photos?offset=114&limit=5")
+    const response = await fetch("https://api.slingacademy.com/v1/sample-data/photos?offset=64&limit=5")
     return await response.json()
 }
 
 function slideHeader(data){
-    document.getElementById("header-bg-"+(bgHeaderID ? 1 : 2)).style.display = "block"
-    document.getElementById("header-bg-"+(bgHeaderID ? 2 : 1)).style.display = "none"
-
-    document.getElementById("header-bg-"+(bgHeaderID ? 1 : 2)).style.backgroundImage = "url(" + data.photos[headerSliderID].url + ")"
     document.getElementById("header-slider-index").innerHTML = (headerSliderID+1)+"/5"
-
-    headerSliderID++
-    if (headerSliderID === 5) headerSliderID = 0
-    bgHeaderID = !bgHeaderID
-
-    document.getElementById("header-bg-"+(bgHeaderID ? 1 : 2)).style.backgroundImage = "url(" + data.photos[headerSliderID].url + ")"
-
     document.getElementById("header-slider-line").animate(
         [
             { width: '0'},
@@ -30,6 +20,49 @@ function slideHeader(data){
             iterations: 1
         }
     )
+
+    switch (h_animationIndex) {
+        case 0:
+            document.getElementById("header-bg-"+1).style.backgroundImage = "url(" + data.photos[headerSliderID].url + ")"
+            headerSliderID++
+            document.getElementById("header-bg-"+2).style.backgroundImage = "url(" + data.photos[headerSliderID].url + ")"
+            h_animationIndex++
+            break
+        case 1:
+            headerSliderID++
+            if (headerSliderID === 5) headerSliderID = 0
+
+            document.getElementById("header-bg-"+(bgHeaderID ? 1 : 2)).style.left = "-100%"
+            document.getElementById("header-bg-"+(bgHeaderID ? 2 : 1)).style.left = "0"
+
+            document.getElementById("header-bg-"+(bgHeaderID ? 1 : 2)).animate(
+                [
+                    { left: '0'},
+                    { left: '100%'}
+                ],
+                {
+                    duration: 1000,
+                    iterations: 1,
+                    easing: "ease-in-out"
+                }
+            )
+            document.getElementById("header-bg-"+(bgHeaderID ? 2 : 1)).animate(
+                [
+                    { left: '-100%'},
+                    { left: '0'}
+                ],
+                {
+                    duration: 1000,
+                    iterations: 1,
+                    easing: "ease-in-out",
+                }
+            )
+            setTimeout(function(){
+                document.getElementById("header-bg-"+(bgHeaderID ? 1 : 2)).style.backgroundImage = "url(" + data.photos[headerSliderID].url + ")"
+                bgHeaderID = !bgHeaderID
+            }, 1000)
+            break
+    }
 }
 
 setHeaderBG().then(r => {
--- resources/scripts/topics_slider.js
+++ resources/scripts/topics_slider.js
@@ -1,5 +1,7 @@
+
 let topicsSliderID = 0;
 let bgTopicsID = true
+let t_animationIndex = 0
 
 const setTopicsBG = async () => {
     const response = await fetch("https://api.slingacademy.com/v1/sample-data/photos?offset=38&limit=5")
@@ -7,18 +9,7 @@ const setTopicsBG = async () => {
 }
 
 function slideTopics(data){
-    document.getElementById("topics-bg-"+(bgTopicsID ? 1 : 2)).style.display = "block"
-    document.getElementById("topics-bg-"+(bgTopicsID ? 2 : 1)).style.display = "none"
-
-    document.getElementById("topics-bg-"+(bgTopicsID ? 1 : 2)).style.backgroundImage = "url(" + data.photos[topicsSliderID].url + ")"
     document.getElementById("topics-slider-index").innerHTML = (topicsSliderID+1)+"/5"
-
-    topicsSliderID++
-    if (topicsSliderID === 5) topicsSliderID = 0
-    bgTopicsID = !bgTopicsID
-
-    document.getElementById("topics-bg-"+(bgTopicsID ? 1 : 2)).style.backgroundImage = "url(" + data.photos[topicsSliderID].url + ")"
-
     document.getElementById("topics-slider-line").animate(
         [
             { width: '0'},
@@ -29,6 +20,48 @@ function slideTopics(data){
             iterations: 1
         }
     )
+
+    switch (t_animationIndex) {
+        case 0:
+            document.getElementById("topics-bg-"+1).style.backgroundImage = "url(" + data.photos[topicsSliderID].url + ")"
+            topicsSliderID++
+            document.getElementById("topics-bg-"+2).style.backgroundImage = "url(" + data.photos[topicsSliderID].url + ")"
+            t_animationIndex++
+            break
+        case 1:
+            topicsSliderID++
+            if (topicsSliderID === 5) topicsSliderID = 0
+
+
+
+            document.getElementById("topics-bg-"+(bgTopicsID ? 1 : 2)).animate(
+                {
+                    transform: "translateX(100%)"
+                },
+                {
+                    duration: 1000,
+                    iterations: 1,
+                    easing: "ease-in-out"
+                }
+            )
+            document.getElementById("topics-bg-"+(bgTopicsID ? 2 : 1)).animate(
+                {
+                    transform: "translateX(0)"
+                },
+                {
+                    duration: 1000,
+                    iterations: 1,
+                    easing: "ease-in-out",
+                }
+            )
+            setTimeout(function(){
+                document.getElementById("topics-bg-"+(bgTopicsID ? 1 : 2)).style.transform = "translateX(-100%)"
+                document.getElementById("topics-bg-"+(bgTopicsID ? 2 : 1)).style.transform = "translateX(0)"
+                document.getElementById("topics-bg-"+(bgTopicsID ? 1 : 2)).style.backgroundImage = "url(" + data.photos[topicsSliderID].url + ")"
+                bgTopicsID = !bgTopicsID
+            }, 1000)
+            break
+    }
 }
 
 setTopicsBG().then(r => {
